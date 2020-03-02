984135cb7bdbf830b514e4bff853f9bfb4bd4f66
xuefeiWan <wxfil@outlook.com>
2020-03-03 00:16:55 +0800

    JS DOM编程艺术7.4.1
    今天从第7章看到7.4.1

--- dom_test1/scripts/showPic.js
+++ dom_test1/scripts/showPic.js
@@ -40,3 +40,19 @@ function addLoadEvent(func) {
 		}
 	}
 }
+
+addLoadEvent(preparePlaceholder);
+function preparePlaceholder() {
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id", "placeholder");
+	placeholder.setAttribute("src", "images/placeholder.jpg");
+	placeholder.setAttribute("alt", "my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id", "description");
+	var text = document.createTextNode("Choose an image.");
+	description.appendChild(text);
+	document.getElementsByTagName("body")[0].appendChild(placeholder);
+	document.getElementsByTagName("body")[0].appendChild(description);
+}
+
+
