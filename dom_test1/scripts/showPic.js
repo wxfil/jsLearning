d444f39d0b062e92addb902c3d6a964556bd8bfe
xuefeiWan <wxfil@outlook.com>
2020-03-03 23:47:17 +0800

    JS DOM编程艺术 第7章结束
    此MR是前面7章所有的图库代码

--- dom_test1/scripts/showPic.js
+++ dom_test1/scripts/showPic.js
@@ -1,4 +1,25 @@
 
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if(typeof window.onload != "function") {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			oldonload();
+			func();
+		}
+	}
+}
+
+function insertAfter(newElement, targetElement) {
+	var parent = targetElement.parentNode;
+	if(parent.lastChild == targetElement) {
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
+
 function showPic(whichpic){
 	if(!document.getElementById("placeholder")) return true;
 	var source = whichpic.getAttribute("href");
@@ -14,7 +35,6 @@ function showPic(whichpic){
 	return false;
 }
 
-//window.onload = prepareGallery;
 function prepareGallery() {
 	if (!document.getElementById) return false;
 	if (!document.getElementsByTagName) return false;
@@ -28,21 +48,11 @@ function prepareGallery() {
 	}
 }
 
-addLoadEvent(prepareGallery);
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if(typeof window.onload != "function") {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			oldonload();
-			func();
-		}
-	}
-}
-
-addLoadEvent(preparePlaceholder);
 function preparePlaceholder() {
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
 	var placeholder = document.createElement("img");
 	placeholder.setAttribute("id", "placeholder");
 	placeholder.setAttribute("src", "images/placeholder.jpg");
@@ -51,8 +61,22 @@ function preparePlaceholder() {
 	description.setAttribute("id", "description");
 	var text = document.createTextNode("Choose an image.");
 	description.appendChild(text);
-	document.getElementsByTagName("body")[0].appendChild(placeholder);
-	document.getElementsByTagName("body")[0].appendChild(description);
+	
+	// 方式1：放在body元素的最后1个节点位置
+	//document.getElementsByTagName("body")[0].appendChild(placeholder);
+	//document.getElementsByTagName("body")[0].appendChild(description);
+	
+	// 方式2：将元素插入gallery元素之前
+	//var gallery = document.getElementById("imagegallery");
+	//gallery.parentNode.insertBefore(placeholder, gallery);
+	//gallery.parentNode.insertBefore(description, gallery);
+	
+	// 方式3：将元素插入gallery元素之后
+	var gallery = document.getElementById("imagegallery"); 
+	insertAfter(placeholder, gallery);
+	insertAfter(description, placeholder);
 }
 
+addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
 
